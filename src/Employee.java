dbe80bbc8a0fb46c171658ec79071539357534c9
wwr2005 <wwr20005@gmail.com>
2023-10-14 00:27:20 +0300

    13

---
+++ src/Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+
+    private String fullName;
+    private int department;
+    private int salary;
+    private int id;
+    private static int counter = 1;
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department =department;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Main {
+
+    private final static Employee[] EMPLOYEES = new Employee[10];
+
+    private static final Random RANDOM = Random();
+    private final static String[] MALE_NAMES = {"Вася", "Петя", "Алёша", "Никита", "Фома"};
+    private final static String[] FEMALE_NAMES = {"Светлана", "Ката", "Марфа", "Анжелика", "Фатима"};
+    private final static String[] MALE_SURNAMES = {"Гольперов", "Сурков", "Иванов", "Журавлёв", "Лошкин"};
+    private final static String[] FEMALE_SURNAMES = {"Галустян", "Мишуткина", "Владимировна", "Суворова", "Попова"};
+
+    private static Employee generateEmployee() {
+        boolean male = RANDOM.nextBoolean();
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EMPLOYEES[0] = new Employee();
+
     }
 }
